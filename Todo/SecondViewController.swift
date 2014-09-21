a618c15d391ecea83dcc3787d074bddc1ba84bfb
anand <anandchitravelu@gmail.com>
2014-09-21 14:09:55 +0530

    Initial Commit

---
+++ Todo/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Todo
+//
+//  Created by Anand on 9/21/14.
+//  Copyright (c) 2014 qruize. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
